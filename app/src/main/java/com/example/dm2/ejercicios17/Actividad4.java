434b4ab853a288defb0180f8f4090d3a1c7387ad
davbladm2 <davidblancoalvarez@gmail.com>
2016-11-02 09:50:24 +0100

    1

--- app/src/main/java/com/example/dm2/ejercicios17/Actividad4.java
+++ app/src/main/java/com/example/dm2/ejercicios17/Actividad4.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 public class Actividad4 extends AppCompatActivity {
 
     EditText editTextNombre,editTextApellidos;
     RadioGroup radioGroupSexo;
     RadioButton radioButtonMasculino,radioButtonFemenino;
+    CheckBox checkBoxMusica,checkBoxLectura,checkBoxDeportes,checkBoxViajar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad4);
@@ -22,10 +25,18 @@ public class Actividad4 extends AppCompatActivity {
         radioGroupSexo=(RadioGroup)findViewById(R.id.radioGroupSexo);
         radioButtonMasculino=(RadioButton)findViewById(R.id.radioButtonMasculino);
         radioButtonFemenino=(RadioButton)findViewById(R.id.radioButtonFemenino);
+        checkBoxMusica=(CheckBox)findViewById(R.id.checkBoxMusica);
+        checkBoxLectura=(CheckBox)findViewById(R.id.checkBoxLectura);
+        checkBoxDeportes=(CheckBox)findViewById(R.id.checkBoxDeportes);
+        checkBoxViajar=(CheckBox)findViewById(R.id.checkBoxViajar);
     }
 
     public void enviar(View v){
         boolean comprobar=true;
+        ArrayList<String> lista=null;
+
+
+        //Comprobaciones de Nombre y Apellidos
         if(editTextNombre.getText().toString().equals("")){
             Toast toast1=Toast.makeText(getApplicationContext(),"Introduce un Nombre", Toast.LENGTH_SHORT);
             toast1.show();
@@ -37,6 +48,8 @@ public class Actividad4 extends AppCompatActivity {
                 comprobar=false;
             }
         }
+
+        //Comprobaciones del radio seleccionado
         int idRadioGroup = radioGroupSexo.getCheckedRadioButtonId();
         int idRadioMasculino=radioButtonMasculino.getId();
         int idRadioFemenino=radioButtonFemenino.getId();
@@ -51,12 +64,29 @@ public class Actividad4 extends AppCompatActivity {
             comprobar=false;
         }
 
+        //Comprobar las aficiones seleccionadas
+        lista=new ArrayList<>();
+        if(checkBoxMusica.isChecked()){
+            lista.add(checkBoxMusica.getText().toString());
+        }
+        if(checkBoxLectura.isChecked()){
+            lista.add(checkBoxLectura.getText().toString());
+        }
+        if(checkBoxDeportes.isChecked()){
+            lista.add(checkBoxDeportes.getText().toString());
+        }
+        if(checkBoxViajar.isChecked()){
+            lista.add(checkBoxViajar.getText().toString());
+        }
+
+        //Comprobar si se ha rellenado bien lo datos y mandar a la otra página
         if(comprobar==true){
             Intent intent = new Intent(Actividad4.this, Actividad4B.class);
             intent.putExtra("nombre",editTextNombre.getText().toString());
             intent.putExtra("apellidos",editTextApellidos.getText().toString());
             intent.putExtra("radioSele",radioSele);
-            startActivityForResult(intent,1234);
+            intent.putExtra("aficiones",lista);
+            startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/example/dm2/ejercicios17/Actividad4B.java
+++ app/src/main/java/com/example/dm2/ejercicios17/Actividad4B.java
@@ -1,13 +1,38 @@
 package com.example.dm2.ejercicios17;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Actividad4B extends AppCompatActivity {
 
-    @Override
+    TextView textResultado;
+    String nombre,apellidos,radioSele;
+    ArrayList<String> lista;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad4_b);
+        textResultado=(TextView)findViewById(R.id.textResultado);
+        Bundle extras=getIntent().getExtras();
+        nombre=extras.getString("nombre");
+        apellidos=extras.getString("apellidos");
+        radioSele=extras.getString("radioSele");
+        lista=extras.getStringArrayList("aficiones");
+        textResultado.setText("Hola "+nombre+" "+apellidos+" tu sexo es: "+radioSele+".");
+        if(lista.size()>0){
+            textResultado.setText(textResultado.getText()+" Tus aficiones son ");
+            for(String res: lista) {
+                textResultado.setText(textResultado.getText()+res+" ");
+            }
+        }else{
+            textResultado.setText(textResultado.getText()+" Y no tienes aficiones.");
+        }
+    }
+    public void volver(View v){
+        finish();
     }
 }
